a977bdcba6c8e233c1ec67aa264d2cf8bef72f7b
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-08-05 15:07:08 +0700

    fix: use env from config folder in `mongoose.ts`

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,12 +1,11 @@
+import { env } from '@/config/env';
 import mongoose from 'mongoose';
 
 declare global {
 	var mongoose: any; // This must be a `var` and not a `let / const`
 }
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
+if (!env.MONGO_URI) {
 	throw new Error(
 		'Please define the MONGODB_URI environment variable inside .env.local',
 	);
@@ -28,7 +27,7 @@ export async function dbConnect() {
 			bufferCommands: false,
 		};
 
-		cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
+		cached.promise = mongoose.connect(env.MONGO_URI, opts).then(mongoose => {
 			return mongoose;
 		});
 	}
