2d6a3517dab4cee99bf47b37f60c6b18c851ae61
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-08-06 12:24:17 +0700

    feat: configure react query

--- src/features/shared/components/providers.tsx
+++ src/features/shared/components/providers.tsx
@@ -1,9 +1,25 @@
 'use client';
 
+import { getQueryClient } from '@/lib/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 export type ProvidersProps = {
 	children: React.ReactNode;
 };
 
 export const Providers = (props: ProvidersProps) => {
-	return props.children;
+	// NOTE: Avoid useState when initializing the query client if you don't
+	//       have a suspense boundary between this and the code that may
+	//       suspend because React will throw away the client on the initial
+	//       render if it suspends and there is no boundary
+	const queryClient = getQueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+
+			{props.children}
+		</QueryClientProvider>
+	);
 };
