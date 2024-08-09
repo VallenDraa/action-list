e9870cf8983d3e27ab3ab0c3eee7564ceb37559a
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-08-09 09:28:34 +0700

    chore: add mocks for next.js and react modules

--- src/testing/setup-tests.ts
+++ src/testing/setup-tests.ts
@@ -4,12 +4,46 @@ import '@testing-library/jest-dom/vitest';
 import { resetTestDb } from './db';
 import { dbConnect } from '@/lib/mongoose';
 
+beforeAll(() => {
+	// Mocking Next.js and React specific modules and libraries
+	vi.mock('server-only', () => {
+		return {};
+	});
+	vi.mock('@t3-oss/env-nextjs', () => {
+		return { createEnv: () => process.env };
+	});
+	vi.mock('react', async original => {
+		const actuals = await original();
+
+		return {
+			...(actuals as []),
+			cache: vi.fn(),
+		};
+	});
+
+	// * Uncomment the following code block if you want to test server actions
+	// vi.mock('next/headers', async original => {
+	// 	const actuals = await original();
+
+	// 	return {
+	// 		...(actuals as []),
+	// 		cookies: () => ({
+	// 			get: vi.fn(),
+	// 			set: vi.fn(),
+	// 		}),
+	// 	};
+	// });
+});
+
 beforeAll(async () => {
 	const mongoConnection = await dbConnect({ uri: process.env.MONGO_URI });
 	resetTestDb(mongoConnection);
 });
 
-// Runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
 	cleanup();
+});
+
+afterAll(() => {
+	vi.restoreAllMocks();
 });
